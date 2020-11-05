7026233ef9a3cfb84e791478db851038bacbce34
Lourine <lourine.millicent@gmail.com>
2020-11-05 17:27:07 +0300

    Add routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { PostListComponent } from './components/post-list/post-list.component';
+import { PostDetailsComponent } from './components/post-details/post-details.component';
+import { AddPostComponent } from './components/add-post/add-post.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'tutorials', pathMatch: 'full' },
+  { path: 'tutorials', component: PostListComponent },
+  { path: 'tutorials/:id', component: PostDetailsComponent },
+  { path: 'add', component: AddPostComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
